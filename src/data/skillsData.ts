7cf46926d48a3fa0f2702481218699b9c869b6e6
Tran Duy Phat <phattranduy00@gmail.com>
2025-11-12 16:35:59 +0700

    update code

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,97 +1,63 @@
 export const skills = [
   {
     name: "HTML",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=html",
   },
-  {
-    name: "CSS",
-    tag: "Frontend",
-    logo: "https://skillicons.dev/icons?i=css",
-  },
+  { name: "CSS", logo: "https://skillicons.dev/icons?i=css" },
   {
     name: "JavaScript",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=javascript",
   },
   {
     name: "TypeScript",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=typescript",
   },
   {
     name: "React JS",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=react",
   },
   {
     name: "Next.js",
-    tag: "Frontend",
-    logo: "https://skillicons.dev/icons?i=nextjs",
+    logo: "https://skillicons.dev/icons?i=nextjs&w=white",
   },
   {
     name: "Redux",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=redux",
   },
   {
     name: "Sass",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=sass",
   },
   {
     name: "Vite",
-    tag: "Frontend",
     logo: "https://skillicons.dev/icons?i=vite",
   },
   {
     name: "TailwindCSS",
-    tag: "UI/UX",
     logo: "https://skillicons.dev/icons?i=tailwind",
   },
   {
     name: "Bootstrap",
-    tag: "UI/UX",
     logo: "https://skillicons.dev/icons?i=bootstrap",
   },
+  { name: "Figma", logo: "https://skillicons.dev/icons?i=figma" },
   {
-    name: "Figma",
-    tag: "UI/UX",
-    logo: "https://skillicons.dev/icons?i=figma",
+    name: "Laravel",
+    logo: "https://skillicons.dev/icons?i=laravel",
   },
-  {
-    name: "PHP",
-    tag: "Backend",
-    logo: "https://skillicons.dev/icons?i=php",
-  },
-  {
-    name: "C#",
-    tag: "Backend",
-    logo: "https://skillicons.dev/icons?i=cs",
-  },
+  { name: "C#", logo: "https://skillicons.dev/icons?i=cs" },
   {
     name: ".NET",
-    tag: "Backend",
     logo: "https://skillicons.dev/icons?i=dotnet",
   },
-  {
-    name: "Git",
-    tag: "Others",
-    logo: "https://skillicons.dev/icons?i=git",
-  },
+  { name: "Git", logo: "https://skillicons.dev/icons?i=git" },
   {
     name: "GitHub",
-    tag: "Others",
-    logo: "https://skillicons.dev/icons?i=github",
+    logo: "https://skillicons.dev/icons?i=github&w=white",
   },
-  {
-    name: "npm",
-    tag: "Others",
-    logo: "https://skillicons.dev/icons?i=npm",
-  },
+  { name: "npm", logo: "https://skillicons.dev/icons?i=npm" },
   {
     name: "MySQL",
-    tag: "Database",
     logo: "https://skillicons.dev/icons?i=mysql",
   },
 ];
