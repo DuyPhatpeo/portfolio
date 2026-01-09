b1bd42a5e309b197d58fa26d359fae45eb913bdf
Tran Duy Phat <phattranduy00@gmail.com>
2026-01-09 10:36:54 +0700

    feat(ui): add ClickSpark component
    - New `ClickSpark` component for interactive clicks
    - Configures `shadcn/ui` setup via `components.json`
    - Adds `cn` utility function for Tailwind class merging

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./GlobalStyle.css";
 import ScrollToTop from "./components/general/ScrollToTop";
+import ClickSpark from "./components/general/ClickSpark";
 
 const AppContent: React.FC = () => {
   const { darkMode } = useThemeStore();
@@ -19,22 +20,32 @@ const AppContent: React.FC = () => {
 
   return (
     <div className="relative min-h-screen bg-transparent">
+      {/* Background */}
       <Particles quantity={50} />
 
-      <div className="relative z-10">
-        <AppRoutes />
-      </div>
+      {/* Click Spark wrapper */}
+      <ClickSpark
+        sparkColor={darkMode ? "#fff" : "#000"}
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <div className="relative z-10">
+          <AppRoutes />
 
-      {/* Scroll To Top */}
-      <ScrollToTop />
+          {/* Scroll To Top */}
+          <ScrollToTop />
 
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        pauseOnHover
-        draggable
-        theme={darkMode ? "dark" : "light"}
-      />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            pauseOnHover
+            draggable
+            theme={darkMode ? "dark" : "light"}
+          />
+        </div>
+      </ClickSpark>
     </div>
   );
 };
